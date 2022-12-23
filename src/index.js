db4abc26945faf64f48e016f1a222ba2e2c5bd89
WaxArsatia <waxarsatia@gmail.com>
2022-12-23 10:44:46 +0700

    lint: Add husky to trigger eslint for commit

--- src/index.js
+++ src/index.js
@@ -5,16 +5,16 @@ const app = express();
 
 app.post('*', (req, res) => {
   res.sendStatus(200);
-  console.log('New Trigger to', req.url)
-  axios.post(`http://host.docker.internal:${process.env.HOST_PORT}` + req.url)
-    .then(res => {
-      console.log('Success to redeploy')
-      console.log(res.data)
+  console.log('New Trigger to', req.url);
+  axios.post(`http://host.docker.internal:${process.env.HOST_PORT}${req.url}`)
+    .then((response) => {
+      console.log('Success to redeploy');
+      console.log(response.data);
     })
-    .catch(err => {
+    .catch((err) => {
       console.warn('Failed to redeploy');
-      console.warn(err.response.data)
+      console.warn(err.response.data);
     });
-})
+});
 
 app.listen(Number(process.env.LISTEN_PORT) || 3000, () => console.log(`Proxy ready on port ${process.env.LISTEN_PORT || 3000}`));
